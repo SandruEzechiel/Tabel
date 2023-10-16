270c8888f4d0dff7f2131cf52c7bbfae4e38878b
SabdruEzechiel <ezechiel.sandru@emanuel.ro>
2023-10-16 22:45:11 +0300

    a

---
+++ Tabel/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Tabel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bitwise AND Truth Table");
+
+            Console.WriteLine("");
+
+            Console.WriteLine("-----------------------");
+
+            Console.WriteLine("|  A  |  B  | A & B |");
+
+            Console.WriteLine("|-----|-----|-------|");
+
+
+            for (int A = 0; A <= 1; A++)
+            {
+                for (int B = 0; B <= 1; B++)
+                {
+                    int result = A & B;
+                    Console.WriteLine($"|  {A}  |  {B}  |   {result}   |");
+                }
+            }
+        }
+    }
+}
